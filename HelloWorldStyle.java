b24277f44d5ff12a04a9464e927543096073790e
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-09 14:54:34 +0000

    Initial commit

---
+++ HelloWorldStyle.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Chris Di Bert All rights reserved.
+// .
+// Created by: Christopher Di Bert
+// Date : Feb.7, 2024
+// This program prints hello to my friend and prints
+// a face
+
+package com.example;
+
+public class HelloWorldStyle {
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, Spencer!");
+    System.out.println("Look at this funny face: ( ͡° ʖ ͡°)");
+  }
+}
