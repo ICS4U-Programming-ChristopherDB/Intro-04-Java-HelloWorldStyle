9ab3679f8237f2fc93047ce59e12222c44c59b40
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-14 18:45:51 +0000

    Added javadocs

---
+++ com/example/HelloWorldStyle.java
@@ -0,0 +1,31 @@
+package com.example;
+
+/**
+ * Says hello to my friend with style.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 14-2-2024
+ */
+
+// HelloWorldStyle class
+public final class HelloWorldStyle {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private HelloWorldStyle() {
+        throw new UnsupportedOperationException
+        ("This class cannot be instantiated.");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, Spencer!");
+        System.out.println("Look at this funny face: ( ͡° ʖ ͡°)");
+    }
+}
